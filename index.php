d75e644a40907d9c94c4726872f6392dae267984
urlordjames <urlordjames@gmail.com>
2019-05-08 20:44:54 -0400

    added chat button and disclaimer

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
 	</head>
 	<body>
 		NOT RELATED IN ANY WAY TO THE PUZZLES ON THIS WEBSITE!<br>
-		<a href="register">register</a><br><a href="login">login</a><br><a href="https://github.com/urlordjames/chatwithamigos">GitHub page</a>
+		please note: error messages are not fully implemented and a page will simply show up blank if you are not logged in!<br>
+		<a href="message">start chatting!</a><br>
+		<a href="register">register</a><br>
+		<a href="login">login</a><br>
+		<a href="https://github.com/urlordjames/chatwithamigos">GitHub page</a>
 	</body>
 </html>
